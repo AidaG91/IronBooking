ecd5f24545912e4f5db3c2efd9bd4a5e4b012470
krub-dev <kikorubioillan@gmail.com>
2025-08-29 09:50:26 +0200

    project struct setup

---
+++ src/main/java/com/ironhack/IronBooking/IronBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.IronBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/IronBooking/IronBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.IronBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
