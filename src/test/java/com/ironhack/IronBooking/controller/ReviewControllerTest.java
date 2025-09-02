44d0c6867e6a1707857db2f00074e8a5a1dfe52c
AidaG91 <aida.garmus@gmail.com>
2025-09-03 00:36:34 +0200

    GetReviewById and CreateReview Tests created

--- src/test/java/com/ironhack/IronBooking/controller/ReviewControllerTest.java
+++ src/test/java/com/ironhack/IronBooking/controller/ReviewControllerTest.java
@@ -1,18 +1,26 @@
 package com.ironhack.IronBooking.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ironhack.IronBooking.dto.review.ReviewRequestDTO;
 import com.ironhack.IronBooking.dto.review.ReviewResponseDTO;
 import com.ironhack.IronBooking.service.interfaces.ReviewService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.net.URI;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.ArgumentMatcher.*;
@@ -54,11 +62,55 @@ class ReviewControllerTest {
     }
 
     @Test
-    void getReviewById() {
+    @DisplayName(("GET /reviews/{id}"))
+    void getReviewById_ok() throws Exception {
+        var review = resp(2L, 3, "We are testing getting review by id", 24L, "Colin", "Firth", 22L, "Barcelona Studio" +
+                " 3");
+
+
+        when(reviewService.getReviewById(2L)).thenReturn(review);
+
+        mvc.perform(get("/reviews/{id}", 2))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(2L));
+
+        verify(reviewService).getReviewById(2L);
     }
 
+    // ------- TESTING POST -------
     @Test
-    void createReview() {
+    @DisplayName("POST")
+    void createReview() throws Exception {
+        var req = new ReviewRequestDTO();
+        req.setRating(4);
+        req.setComment("Testing that this will be posted.");
+        req.setPlaceId(1L);
+        req.setUserId(11L);
+
+        var created = resp(1L, 4, "Testing that this will be posted.", 11L, "Test", "User", 1L, "Test Place");
+        when(reviewService.createReview(any())).thenReturn(created);
+
+
+        var result = mvc.perform(
+                        post("/reviews")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(om.writeValueAsString(req))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/reviews/1"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.comment").value("Testing that this will be posted."))
+                .andReturn();
+
+
+        ArgumentCaptor<ReviewRequestDTO> captor = ArgumentCaptor.forClass(ReviewRequestDTO.class);
+        verify(reviewService).createReview((captor.capture()));
+        var sent = captor.getValue();
+        assertThat(sent.getRating()).isEqualTo(4);
+        assertThat(sent.getComment()).isEqualTo("Testing that this will be posted.");
+
     }
 
     @Test
